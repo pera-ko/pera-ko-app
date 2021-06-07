de0f8cd190dd844fe50da108aa960b437419f421
Joel Manuel <joel.manuel@infor.com>
2021-06-07 20:54:25 +0800

    complete budget form

--- src/components/budget-form.tsx
+++ src/components/budget-form.tsx
@@ -1,33 +1,70 @@
-import { RadioGroup } from "@headlessui/react"
+import { Popover, RadioGroup } from "@headlessui/react"
 import React, { Fragment, useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { HexColorPicker } from "react-colorful";
 import BudgetRadio from "./budget-radio"
+import InputGroup from "./input-group"
+import {default as EmojiPicker, IEmojiData } from "emoji-picker-react";
 
 interface Budget {
   type: "budget";
   budgetName: string;
   amount: number;
+  icon: string;
 }
 
 interface Goal {
   type: "goal";
   budgetName: string;
   amount: number;
+  icon: string;
   startDate: string;
   endDate?: string | undefined;
   installmentType: "monthly" | "semi-monthly"
 }
 
+interface Inputs {
+  budgetName: string;
+  amount: number;
+  icon: string;
+  startDate?: string;
+  endDate?: string;
+  installmentType?: "monthly" | "semi-monthly"
+}
+
 interface BudgetFormProps {
   defaultValue?: Budget | Goal
+  onSubmit?(value: Budget | Goal): void
 }
-const BudgetForm: React.FC<BudgetFormProps> = ({ defaultValue }) => {
-  const { register, handleSubmit, formState: { errors } } = useForm<Budget | Goal>({defaultValues: defaultValue});
+
+const BudgetForm: React.FC<BudgetFormProps> = ({ defaultValue, onSubmit }) => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({defaultValues: defaultValue});
   const [type, setType] = useState<"budget" | "goal">("budget")
   const [installmentType, setInstallmentType] = useState<"monthly" | "semi-monthly">("monthly")
+  const [chosenEmoji, setChosenEmoji] = useState<IEmojiData | null>();
+  const [color, setColor] = useState("#fff");
 
-  const handleFormSubmit:SubmitHandler<Budget | Goal> = (data) => {
-    console.log(data)
+  const handleFormSubmit:SubmitHandler<Inputs> = (data) => {
+    let returnValue: Budget | Goal;
+    if (type === "budget") {
+      returnValue = {
+        type,
+        budgetName: data.budgetName,
+        amount: data.amount,
+        icon: chosenEmoji?.emoji!
+      }
+    } else {
+      returnValue = {
+        type,
+        budgetName: data.budgetName,
+        amount: data.amount,
+        icon: chosenEmoji?.emoji!,
+        startDate: data.startDate!,
+        endDate: data.endDate,
+        installmentType
+      }
+    }
+    if (onSubmit) onSubmit(returnValue)
   }
 
   return (
@@ -39,58 +76,58 @@ const BudgetForm: React.FC<BudgetFormProps> = ({ defaultValue }) => {
         <div className='w-11 mr-3'>
           <label className='block mb-2'>
             <div className='font-medium text-sm my-1 text-gray-600'>Icon</div>
-            <button 
-              type='button'
-              className='block h-11 w-11 px-3 font-medium items-center bg-indigo-100 rounded-md border-2 border-transparent focus-within:border-indigo-300'>
-            </button>
+            <Popover>
+              <Popover.Button type='button'
+              className='block h-11 w-11 text-2xl font-medium items-center bg-indigo-100 rounded-md border-2 border-transparent focus:border-indigo-300 outline-none focus:outline-none'>
+                {chosenEmoji
+                  ? chosenEmoji.emoji
+                  : defaultValue
+                  ? defaultValue.icon
+                  : ''}
+              </Popover.Button>
+              <Popover.Panel className="absolute z-10">
+                <EmojiPicker
+                  onEmojiClick={(e, emoji) => setChosenEmoji(emoji)}
+                  native
+                />
+              </Popover.Panel>
+            </Popover>
           </label>
           <label className=''>
             <div className='font-medium text-sm my-1 text-gray-600'>Color</div>
-            <button 
-              type='button'
-              className='block h-11 w-11 px-3 font-medium items-center bg-indigo-100 rounded-full border-2 border-transparent focus-within:border-indigo-300'>
-            </button>
+            <Popover>
+              <Popover.Button type='button'
+              className='block h-11 w-11 px-3 rounded-full border-2 focus:border-indigo-300 focus:outline-none outline-none' style={{backgroundColor: color}}></Popover.Button>
+              <Popover.Panel className="absolute z-10">
+                <HexColorPicker color={color} onChange={setColor}/>
+              </Popover.Panel>
+            </Popover>
           </label>
         </div>
         <div className='flex-1'>
-          <label className='block mb-2'>
-            <div className='font-medium text-sm my-1 text-gray-600'>Name</div>
-            <div className={`
-              ${errors.budgetName ? 'border-red-400' : 'focus-within:border-indigo-300'}
-              px-3 font-medium flex items-center bg-indigo-100 rounded-md 
-              border-2 border-transparent `}>
-              <input 
-                {...register("budgetName", { required: "Name is required."})}
-                className='font-medium flex-1 outline-none focus:outline-none py-2 bg-transparent w-full'/>
-            </div>
-            {errors.budgetName && <span className='text-xs text-red-700'>{errors.budgetName?.message}</span>}
-          </label>
-          <label className='block mb-2'>
-            <div className='font-medium text-sm my-1 text-gray-600'>Amount</div>
-            <div className={`
-              ${errors.amount ? 'border-red-400' : 'focus-within:border-indigo-300'}
-              px-3 font-medium flex items-center bg-indigo-100 rounded-md 
-              border-2 border-transparent `}>
-              <span className='mr-3 text-gray-600'>PHP</span>
-              <input 
-                {...register("amount", { required: "Amount is required."})}
-                className='w-full font-medium flex-1 outline-none focus:outline-none text-right py-2 bg-transparent' type="number" defaultValue={1000.01} step=".01"/>
-            </div>
-          </label>
+          <InputGroup
+            label="Budget Name"
+            error={errors.budgetName}
+            {...register("budgetName", { required: "Name is required."})}
+            />
+          <InputGroup
+            label="Amount"
+            contentLabel={{ type: "text", content: "PHP" }}
+            error={errors.amount}
+            inputClassName="text-right"
+            {...register("amount", { required: "Amount is required."})}/>
           {type === "goal" && (
             <Fragment>
-              <label className='block mb-2'>
-                <div className='font-medium text-sm my-1 text-gray-600'>Start Date</div>
-                <div className=' px-3 font-medium flex items-center bg-indigo-100 rounded-md border-2 border-transparent focus-within:border-indigo-300'>
-                  <input className='w-full font-medium flex-1 outline-none focus:outline-none py-2 bg-transparent' type="date"/>
-                </div>
-              </label>
-              <label className='block mb-2'>
-                <div className='font-medium text-sm my-1 text-gray-600'>End Date</div>
-                <div className=' px-3 font-medium flex items-center bg-indigo-100 rounded-md border-2 border-transparent focus-within:border-indigo-300'>
-                  <input className='w-full font-medium flex-1 outline-none focus:outline-none py-2 bg-transparent' type="date"/>
-                </div>
-              </label>
+              <InputGroup
+                label="Start Date"
+                error={errors.startDate}
+                type="date"
+                {...register("startDate", { required: "Start Date is required.", setValueAs: v => new Date(v).toJSON() })}/>
+              <InputGroup
+                label="End Date"
+                error={errors.endDate}
+                type="date"
+                {...register("endDate", { setValueAs: v => new Date(v).toJSON() })}/>
               <div className='font-medium text-sm my-1 text-gray-600 mt-1'>Installment Type</div>
               <RadioGroup value={installmentType} onChange={setInstallmentType} className='grid grid-cols-2 text-center text-sm font-medium focus:bg-black'>
                 <RadioGroup.Option value="monthly"
---
+++ src/components/input-group.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { InputHTMLAttributes } from "react";
+import { FieldError } from "react-hook-form";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error?: FieldError;
+  contentLabel?: {
+    type: "text",
+    content: string
+  },
+  inputClassName?: string
+}
+
+function InputGroup(
+  { label, error, className, contentLabel, inputClassName, ...rest }: Props,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
+  return (
+    <label className={`block mb-2 ${className}`}>
+      <div className='font-medium text-sm my-1 text-gray-600'>{label}</div>
+      <div className={`
+        ${error ? 'border-red-400' : 'focus-within:border-indigo-300'}
+        px-3 font-medium flex items-center bg-indigo-100 rounded-md 
+        border-2 border-transparent `}>
+        {contentLabel && (contentLabel.type === "text" ? (
+          <span className='mr-3 text-gray-600'>{contentLabel.content}</span>
+        ) : null)}
+        <input 
+          ref={ref}
+          className={`font-medium flex-1 outline-none focus:outline-none py-2 bg-transparent w-full ${inputClassName}`}
+          {...rest}/>
+      </div>
+      {error && <span className='text-xs text-red-700'>{error?.message}</span>}
+    </label>
+  )
+}
+
+export default React.forwardRef(InputGroup);
