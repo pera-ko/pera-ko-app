1d9f323306a9e3e0e3d34c78044305d48003321b
Joel Manuel <aysfrag@gmail.com>
2021-06-19 17:44:08 +0800

    fixed formatting of money

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,4 +1,5 @@
 export function money(value: number, currency: string = "PHP") {
+  value = Number(value)
   return currency + ' ' + value.toLocaleString('en-us', {
     minimumFractionDigits: 2,
     currency: currency
