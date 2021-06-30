0592dbf0a867e321b8e2a3fcbff48e8eb3a47f25
Joel Manuel <joel.manuel@infor.com>
2021-06-30 22:52:11 +0800

    added empty state for income

--- src/containers/income.tsx
+++ src/containers/income.tsx
@@ -1,8 +1,10 @@
 import { ArrowCircleDownIcon, ArrowLeftIcon } from '@heroicons/react/outline';
+import { ReactComponent as WalletPana } from '../assets/svg/Wallet-pana.svg';
 import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
 import { Fragment } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { IIncome } from '../app/@types';
 import { useTransactionStore } from '../app/store';
 import { money } from '../app/utils';
 
@@ -12,8 +14,6 @@ export default function Income() {
   const { year, month } = useParams<{ year: string; month: string }>();
   const { incomeList } = useTransactionStore(+year, +month)((state) => state);
 
-  let lastDate: string | null = null;
-
   return (
     <Fragment>
       <div className='sticky h- top-0 bg-white flex items-center font-medium'>
@@ -22,39 +22,21 @@ export default function Income() {
         </Link>
         Income
       </div>
-      <ul>
-        {incomeList.map((income, index) => {
-          var retVal: React.ReactElement[] = [];
-          var currentDate = new Date(income.tranDate).toDateString();
-
-          if (lastDate !== currentDate) {
-            var desc = dayjs(currentDate).calendar(undefined, {
-              sameDay: '[Today]',
-              lastDay: '[Yesterday]'
-            });
-            retVal.push(
-              <IncomeHeader key={desc} date='Friday, June 30, 2021' />
-            );
-            lastDate = currentDate;
-          }
-
-          retVal.push(
-            <IncomeItem
-              key={income.tranDate}
-              amount={income.amount}
-              remarks={income.remarks}
-            />
-          );
-          return <Fragment key={index}>{retVal}</Fragment>;
-        })}
-      </ul>
-      <Link
-        to={`/${year}/${month}/income/new`}
-        className='fixed bottom-5 right-5 text-sm font-medium p-3 bg-indigo-500 text-white rounded-full shadow-md'
-      >
-        <ArrowCircleDownIcon className='h-6 w-6 mr-3 inline-block' />
-        Add Income
-      </Link>
+      {incomeList.length > 0 ? (
+        <>
+          <List incomeList={incomeList} />
+          <AddIncomeButton
+            to={`/${year}/${month}/income/new`}
+            className='fixed bottom-5 right-5'
+          />
+        </>
+      ) : (
+        <div className='text-center'>
+          <WalletPana className='w-96' />
+          <div className='mb-5 -mt-3 font-medium'>No Income yet</div>
+          <AddIncomeButton to={`/${year}/${month}/income/new`} />
+        </div>
+      )}
     </Fragment>
   );
 }
@@ -78,5 +60,53 @@ function IncomeItem({ amount, remarks }: IncomeItemProps) {
         <div className='text-xs text-gray-600'>{remarks}</div>
       </div>
     </li>
+  );
+}
+
+function List({ incomeList }: { incomeList: IIncome[] }) {
+  let lastDate: string | null = null;
+  return (
+    <ul>
+      {incomeList.map((income, index) => {
+        var retVal: React.ReactElement[] = [];
+        var currentDate = new Date(income.tranDate).toDateString();
+
+        if (lastDate !== currentDate) {
+          var desc = dayjs(currentDate).calendar(undefined, {
+            sameDay: '[Today]',
+            lastDay: '[Yesterday]'
+          });
+          retVal.push(<IncomeHeader key={desc} date='Friday, June 30, 2021' />);
+          lastDate = currentDate;
+        }
+
+        retVal.push(
+          <IncomeItem
+            key={income.tranDate}
+            amount={income.amount}
+            remarks={income.remarks}
+          />
+        );
+        return <Fragment key={index}>{retVal}</Fragment>;
+      })}
+    </ul>
+  );
+}
+
+function AddIncomeButton({
+  to,
+  className
+}: {
+  to: string;
+  className?: string;
+}) {
+  return (
+    <Link
+      to={to}
+      className={`${className} text-sm font-medium p-3 bg-indigo-500 text-white rounded-full shadow-md`}
+    >
+      <ArrowCircleDownIcon className='h-6 w-6 mr-3 inline-block' />
+      Add Income
+    </Link>
   );
 }
