582e1c86831dc693b2bc334c5305fa9aa9580b6e
Joel Manuel <devjoelmanuel@gmail.com>
2022-03-15 21:59:42 +0800

    added className support for StickyHeader component

--- src/components/sticky-header.tsx
+++ src/components/sticky-header.tsx
@@ -1,6 +1,11 @@
-const StickyHeader: React.FC = ({ children }) => {
+const StickyHeader: React.FC<{ className?: string }> = ({
+  children,
+  className
+}) => {
   return (
-    <div className='bg-gray-200 px-5 py-2 text-sm sticky top-16 font-medium flex justify-between'>
+    <div
+      className={`bg-gray-200 px-5 py-2 text-sm sticky top-16 font-medium flex justify-between ${className}`}
+    >
       {children}
     </div>
   );
