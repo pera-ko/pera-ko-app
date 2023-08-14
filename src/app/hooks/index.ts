b267fc1625bf53106df4b75ca05ae0c1778e881d
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-14 21:31:39 +0800

    uselocalstorage hook

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -1,5 +1,27 @@
+import React from "react";
 import { useLocation } from "react-router";
 
 export function useLocQuery() {
   return new URLSearchParams(useLocation().search);
+}
+
+export function useLocalStorage<T>(name: string, initialValue: T): [T, (initialValue: T) => void] {
+  const [value, setValue] = React.useState<T>(initialValue)
+
+  const localStorageName = `settings-${name}`
+
+  React.useEffect(() => {
+    const storedValue = localStorage.getItem(localStorageName)
+
+    if (storedValue) {
+      setValue(JSON.parse(storedValue))
+    }
+  }, [])
+
+  const saveItem = (val: T) => {
+    localStorage.setItem(localStorageName, JSON.stringify(val))
+    setValue(val)
+  }
+
+  return [value, saveItem]
 }
