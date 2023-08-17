13605f4b63b8ff4c16b56e514108b68ec4a8d118
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-17 14:42:09 +0800

    fixed warnings

--- src/containers/dashboard2.tsx
+++ src/containers/dashboard2.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react"
 import TotalExpenses from "../components/widgets/total-expenses"
 import AppBar from "../components/appbar"
 import NewTransaction from "./new-transaction"
-import TotalPaymentExpenses from "../components/widgets/total-payment-expenses"
+// import TotalPaymentExpenses from "../components/widgets/total-payment-expenses"
 import RecentExpenses from "../components/widgets/recent-expenses"
 
 const Dashboard2 : React.FC<PropsWithChildren> = ({ children }) => {
