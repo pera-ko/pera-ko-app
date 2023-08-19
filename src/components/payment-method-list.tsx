283255ab2c1c964ffb68939445c0a0dd31988df7
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-19 18:20:40 +0800

    payment method list redesign

--- src/components/payment-method-list.tsx
+++ src/components/payment-method-list.tsx
@@ -1,4 +1,3 @@
-import { CheckCircleIcon } from "@heroicons/react/24/outline"
 import { IWalletData } from "../app/@types"
 import PaymentMethodIcon from "./payment-method-icon"
 
@@ -16,26 +15,37 @@ const PaymentMethodList = ({ items, selected, onSelect } : Props) => {
           <li className='block' key={wallet.id}>
             <button
               className={`
-              flex justify-between w-full text-left py-3 px-3 rounded-lg font-medium outline-none focus:outline-none
-              ${
-                wallet.id === selected?.id
-                  ? 'bg-indigo-500/25 text-white'
-                  : 'focus:bg-indigo-50 focus:ring-2 focus:ring-indigo-200'
-              }`}
+              flex justify-between w-full text-left py-2 px-3 font-medium text-sm outline-none focus:outline-none 
+              `}
               onClick={e => {
                 e.preventDefault()
                 onSelect(wallet)
               }}
             >
-              <div className="flex space-x-2">
+              <div className="flex items-center space-x-4">
                 <PaymentMethodIcon type={wallet.type}/>
                 <span>{wallet.walletName}</span>
               </div>
-              {wallet.id === selected?.id && <CheckCircleIcon className='w-6 h-6' />}
+              {wallet.id === selected?.id && <CheckIcon className="w-6 h-6" />}
             </button>
           </li>
         ))}
     </ul>
+  )
+}
+
+function CheckIcon(props: { className : string }) {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" {...props}>
+      <circle cx={12} cy={12} r={12} fill="#22c55e" opacity="0.75" />
+      <path
+        d="M7 13l3 3 7-7"
+        stroke="#fff"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   )
 }
 
