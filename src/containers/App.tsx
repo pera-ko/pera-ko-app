ba72f8c71e42d7067bed2d91e545f45fa2a772ee
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-24 22:12:18 +0800

    future menu

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -5,14 +5,15 @@ import {
   useParams
 } from 'react-router-dom';
 import {
-  Cog6ToothIcon,
-  HomeIcon,
   PlusIcon,
 } from '@heroicons/react/24/outline';
 import { useLocQuery, useLocalStorage } from '../app/hooks';
 import { Fragment, PropsWithChildren } from 'react';
 import Dashboard from './dashboard';
 import Dashboard2 from './dashboard2';
+import { Cog6ToothIcon, 
+  // CurrencyDollarIcon, FlagIcon,
+  HomeIcon } from '@heroicons/react/24/solid';
 
 const App: React.FC<PropsWithChildren> = ({ children }) => {
   const {value: newDashboard, loading } = useLocalStorage('expenses-dashboard', false);
@@ -48,25 +49,29 @@ export function BottomNav() {
   return (
     <Fragment>
       <div className='flex justify-center'>
-        <div className='fixed bottom-8 grid grid-cols-3 backdrop-blur bg-slate-100/30 dark:bg-[#242424]/30 shadow rounded-2xl space-x-3 px-1'>
+        <div className='fixed bottom-8 flex justify-between space-x-3 px-3 backdrop-blur bg-slate-100/30 dark:bg-[#242424]/30 shadow rounded-2xl '>
           <Link to='/' className='flex items-center justify-center px-2 py-2 text-center w-15'>
             <HomeIcon className='inline w-6 h-6' />
-            {/* <div className='text-xs font-medium'></div> */}
           </Link>
+          {/* <Link to='/' className='flex items-center justify-center px-2 py-2 text-center w-15'>
+            <FlagIcon className='inline w-6 h-6' />
+          </Link> */}
           <div className='text-center'>
             <button
-              className='p-4 text-white transform scale-110 rounded-full shadow-md bg-indigo-600/75 backdrop-blur'
+              className='p-4 mx-2 text-white transform scale-110 rounded-full shadow-md bg-indigo-600/75 backdrop-blur'
               onClick={handleNewClick}
             >
               <PlusIcon className='w-6 h-6' />
             </button>
           </div>
+          {/* <Link to='/' className='flex items-center justify-center px-2 py-2 text-center w-15'>
+            <CurrencyDollarIcon className='inline w-6 h-6' />
+          </Link> */}
           <Link
             to={`/${year}/${month}/preferences`}
-            className='flex items-center justify-center px-2 py-2 text-center w-15'
+            className='flex items-center justify-center px-1 py-2 text-center w-15'
           >
             <Cog6ToothIcon className='inline w-6 h-6' />
-            {/* <div className='text-xs font-medium'>Settings</div> */}
           </Link>
         </div>
       </div>
