240608bc4a8226391332b7ad1e287e3fa607705d
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-25 15:28:54 +0800

    use expenselist component

--- src/containers/transactions.tsx
+++ src/containers/transactions.tsx
@@ -1,10 +1,7 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import React, { Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
-import { money } from '../app/utils';
-import BudgetIcon from '../components/budget-icon';
 import { shallow } from 'zustand/shallow';
 import useTransactionStore from '../app/store/transaction-store';
 import useBudgetStore from '../app/store/budget-store';
@@ -13,6 +10,7 @@ import { ITransaction } from '../app/@types';
 import Page from '../components/page';
 import { useLocalStorage } from '../app/hooks';
 import OverviewPie from '../components/widgets/charts/overview-pie';
+import ExpenseList from '../components/expense-list';
 
 dayjs.extend(calendar);
 
@@ -32,7 +30,6 @@ export default function Transactions() {
     ...transactionList.filter((t) => t.type === undefined)
   ] as ITransaction[];
   sortedList.reverse();
-  let lastDate: string | null = null;
 
   const content = (
     <>
@@ -45,52 +42,12 @@ export default function Transactions() {
         title='Expenses'
         />
       <OverviewPie/>
-      <ul>
-        {sortedList.map((t, index) => {
-          var retVal: React.ReactElement[] = [];
-          var currentDate = new Date(t.tranDate).toDateString();
-          const budget = budgetList.find((b) => b.id === t.budgetId);
-          if (!budget) return null;
-          if (lastDate !== currentDate) {
-            var desc = dayjs(currentDate).calendar(undefined, {
-              sameDay: '[Today]',
-              lastDay: '[Yesterday]',
-              lastWeek: '[Last] dddd'
-            });
-            retVal.push(
-              <li
-                key={desc}
-                className='sticky px-5 py-2 text-xs bg-gray-200 dark:bg-zinc-900 top-16'
-              >
-                {desc}
-              </li>
-            );
-            lastDate = currentDate;
-          }
-
-          retVal.push(
-            <li key={t.tranDate} className='flex items-center justify-between'>
-              <div className='flex items-center'>
-                <BudgetIcon className='ml-4 mr-2' color={budget.color} icon={budget.icon} />
-                <div>
-                  <div className='text-sm font-medium'>
-                    {budget?.budgetName}
-                  </div>
-                  <div className='text-xs'>
-                    {walletList[t.walletId].walletName}
-                  </div>
-                </div>
-              </div>
-              <div className='mr-5 text-right'>
-                <div className='text-sm font-medium'>{money(t.amount)}</div>
-                <div className='text-xs text-gray-500'>{t.remarks}</div>
-              </div>
-            </li>
-          );
-
-          return <Fragment key={index}>{retVal}</Fragment>;
-        })}
-      </ul>
+      <ExpenseList 
+        budgetList={budgetList}
+        items={sortedList}
+        walletList={walletList}
+        showHeader
+        />
     </>
   )
 
