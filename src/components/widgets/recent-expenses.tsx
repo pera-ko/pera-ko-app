3b2fca02fc483e0f1e8c9f22ad6f7f8ff47da261
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-27 13:53:01 +0800

    recent expenses bind

--- src/components/widgets/recent-expenses.tsx
+++ src/components/widgets/recent-expenses.tsx
@@ -1,20 +1,13 @@
-import { shallow } from "zustand/shallow"
-import { useBudgetStore } from "../../app/store"
 import useAggregateStore from "../../app/store/aggregate-store"
 import ExpenseList from "../expense-list"
 import WidgetPanel from "../widget-panel"
 
 const RecentExpenses = () => {
   const last10 = useAggregateStore(state => state.transactions.last10)
-  const { budget, wallets } = useBudgetStore(state => ({ budget: state.budget.list, wallets: state.wallet.list }), shallow)
 
   return (
     <WidgetPanel title="RECENT EXPENSES" sticky>
-      <ExpenseList
-        items={last10}
-        budgetList={budget}
-        walletList={wallets}
-        />
+      <ExpenseList items={last10} />
     </WidgetPanel>
   )
 }
