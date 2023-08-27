f856aa3e1d5c6ff1167420b66bc43f817b3eeebe
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-27 13:52:39 +0800

    decoupled overviewpie version

--- src/components/widgets/charts/overview-pie.tsx
+++ src/components/widgets/charts/overview-pie.tsx
@@ -1,8 +1,48 @@
 import { Cell, Pie, PieChart } from "recharts"
-import { useBudgetStore, useTransactionStore } from "../../../app/store";
+import { sumPerCategory, useBudgetStore, useTransactionStore } from "../../../app/store";
 import WidgetPanel from "../../widget-panel";
+import { ITransaction } from "../../../app/@types";
+import { ReactNode } from "react";
 
-const OverviewPie = () => {
+export const OverviewPie = ({ items, rightElement } : { items: ITransaction[], rightElement: ReactNode }) => {
+  const budgetList = useBudgetStore((state) => state.budget.list);
+  const chartData = sumPerCategory(items);
+
+  const colors = chartData.map(
+    (data) => budgetList.find((b) => b.id === data.name)!.color
+  );
+
+  return (
+    <WidgetPanel title="Overview" topRightElement={rightElement}>
+      <div className="flex justify-center my-5">
+        <PieChart className='inline-block' width={300} height={200}>
+          <Pie
+            data={chartData}
+            cy={85}
+            innerRadius={50}
+            outerRadius={70}
+            fill='#8884d8'
+            paddingAngle={5}
+            dataKey='value'
+            label={(e) => {
+              return budgetList.find(b => b.id === e.name)?.icon
+            }}
+          >
+            {chartData.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={colors[index % colors.length]}
+                strokeWidth={0.5}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </div>
+    </WidgetPanel>
+  )
+}
+
+const OverviewPieConnected = () => {
   const budgetList = useBudgetStore((state) => state.budget.list);
   const {
     getTotalOfEachBudget
@@ -43,4 +83,4 @@ const OverviewPie = () => {
   )
 }
 
-export default OverviewPie
+export default OverviewPieConnected
