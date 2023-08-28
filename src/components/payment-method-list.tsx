13498867c3b193bdbc27b7010c884a047feade43
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-28 17:04:40 +0800

    extract checkicon component to separate file

--- src/components/payment-method-list.tsx
+++ src/components/payment-method-list.tsx
@@ -1,4 +1,5 @@
 import { IWalletData } from "../app/@types"
+import CheckIcon from "./circle-check-icon"
 import PaymentMethodIcon from "./payment-method-icon"
 
 type Props = {
@@ -31,21 +32,6 @@ const PaymentMethodList = ({ items, selected, onSelect } : Props) => {
           </li>
         ))}
     </ul>
-  )
-}
-
-function CheckIcon(props: { className : string }) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" {...props}>
-      <circle cx={12} cy={12} r={12} fill="#22c55e" opacity="0.75" />
-      <path
-        d="M7 13l3 3 7-7"
-        stroke="#fff"
-        strokeWidth={1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
   )
 }
 
