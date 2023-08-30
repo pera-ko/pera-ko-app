6b905f954bfc2399dbcc2ae7f4468928d8fd92f0
Joel Manuel <devjoelmanuel@gmail.com>
2023-08-30 20:54:20 +0800

    chip active prop

--- src/components/chip.tsx
+++ src/components/chip.tsx
@@ -6,14 +6,17 @@ type ChipProps = {
   rightElement?: React.ReactNode
   className?: string
   children: string
+  active?: boolean
 }
 
-const Chip = ({ leftElement, rightElement, className, children, onClick } : ChipProps) => {
+const Chip = ({ leftElement, rightElement, className, children, onClick, active } : ChipProps) => {
+  
   return (
     <button 
       type='button'
       className={twMerge(`flex items-center px-2 py-1 text-sm rounded-full border 
       bg-gray-500/10 border-gray-500/30 space-x-1`, 
+        active ? 'border-transparent bg-indigo-500/25' : '',
         className
         )}
       onClick={onClick}
